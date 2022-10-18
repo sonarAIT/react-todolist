df8b3daeb9802e6d186c8be6a0f99e32f798e3ed
sonarAIT <sonarmikumiku@gmail.com>
2022-10-18 20:28:35 +0900

    hook-form

--- pages/TodoInput.tsx
+++ pages/TodoInput.tsx
@@ -7,52 +7,87 @@ import InputLabel from "@mui/material/InputLabel";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
+import { useForm, SubmitHandler, SubmitErrorHandler } from "react-hook-form";
+
+interface TodoForm {
+    taskName: string;
+    taskDetail: string;
+}
+
 const TodoInput: NextComponentType = () => {
+    const isValid: SubmitHandler<TodoForm> = (data: TodoForm) => {
+        console.log(data);
+    };
+    const isInValid: SubmitErrorHandler<TodoForm> = (errors: any) => {
+        console.log(errors);
+        console.log("Fail Login");
+    };
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<TodoForm>();
+
     return (
         <Grid container justifyContent="center">
             <Grid item xs={11}>
                 <Card>
                     <CardContent>
-                        <Grid container rowSpacing={2}>
-                            <Grid item xs={12}>
-                                <Typography
-                                    gutterBottom
-                                    variant="h5"
-                                    component="div"
-                                >
-                                    Input Your Todo
-                                </Typography>
-                            </Grid>
+                        <form
+                            onSubmit={handleSubmit(isValid, isInValid)}
+                        >
+                            <Grid container rowSpacing={2}>
+                                <Grid item xs={12}>
+                                    <Typography
+                                        gutterBottom
+                                        variant="h5"
+                                        component="div"
+                                    >
+                                        Input Your Todo
+                                    </Typography>
+                                </Grid>
 
-                            <Grid item xs={12}>
-                                <InputLabel htmlFor="task-name">
-                                    Task Name
-                                </InputLabel>
-                                <TextField
-                                    id="task-name"
-                                    variant="outlined"
-                                    fullWidth
-                                />
-                            </Grid>
+                                <Grid item xs={12}>
+                                    <InputLabel htmlFor="task-name">
+                                        Task Name
+                                    </InputLabel>
+                                    <TextField
+                                        {...register("taskName", {
+                                            required: "Input Task Name",
+                                            minLength: {
+                                                value: 1,
+                                                message: "Task Name is empty",
+                                            },
+                                        })}
+                                        error={errors.taskName ? true : false}
+                                        helperText={errors.taskName? errors.taskName.message : ""}
+                                        id="task-name"
+                                        variant="outlined"
+                                        fullWidth
+                                    />
+                                </Grid>
 
-                            <Grid item xs={12}>
-                                <InputLabel htmlFor="task-detail">
-                                    Task Detail
-                                </InputLabel>
-                                <TextField
-                                    id="task-detail"
-                                    multiline
-                                    rows={4}
-                                    fullWidth
-                                />
-                            </Grid>
+                                <Grid item xs={12}>
+                                    <InputLabel htmlFor="task-detail">
+                                        Task Detail
+                                    </InputLabel>
+                                    <TextField
+                                        {...register("taskDetail", {
+                                        })}
+                                        id="task-detail"
+                                        multiline
+                                        rows={4}
+                                        fullWidth
+                                    />
+                                </Grid>
 
-                            <Grid item xs={12}>
-                                <Grid container justifyContent="right">
-                                    <Button>Submit</Button>
+                                <Grid item xs={12}>
+                                    <Grid container justifyContent="right">
+                                        <Button type="submit">Submit</Button>
+                                    </Grid>
                                 </Grid>
                             </Grid>
-                        </Grid>
+                        </form>
                     </CardContent>
                 </Card>
             </Grid>
